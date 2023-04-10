1cb592dc463e6bf82bf1b578d6f48d691139280f
magdapaicu <102292971+magdapaicu@users.noreply.github.com>
2023-04-10 13:58:07 +0300

    finish the login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -8,17 +8,9 @@ import { SignupComponent } from './components/signup/signup.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    SignupComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FontAwesomeModule
-  ],
+  declarations: [AppComponent, LoginComponent, SignupComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  type: string = 'password';
+  isText: boolean = false;
+  eyeIcon: string = 'fa-solid fa-eye-slash';
 
+  constructor() {}
+
+  ngOnInit() {}
+
+  hiddenPassord() {
+    this.isText = !this.isText;
+    this.isText
+      ? (this.eyeIcon = 'fa-solid fa-eye')
+      : (this.eyeIcon = 'fa-solid fa-eye-slash');
+    this.isText ? (this.type = 'text') : (this.type = 'password');
+  }
 }
